7fcb66ad47229e0b63f2badda28caaf98b3beed5
Oskar Grzelak <business.oskargrzelak@gmail.com>
2023-12-30 13:38:14 +0100

    Changed the loader hiding after the all hero imgs will load

--- src/Components/HeroesFeatured/HeroesFeatured.js
+++ src/Components/HeroesFeatured/HeroesFeatured.js
@@ -7,44 +7,45 @@ import Loader from '../Loader/Loader';
 const featuredHeroesIds = [4, 253, 391, 146, 121, 131]
 
 function HeroesFeatured() {
-    const [ featuredHeroesList, setFeaturedHeroesList ] = useState([]);
-    const [ isLoading, setLoadingState ] = useState(true);
+    const [featuredHeroesList, setFeaturedHeroesList] = useState([]);
+    const [isLoading, setLoadingState] = useState(true);
 
     useEffect(() => {
         fetchAndRenderFeaturedHeroes();
-    }, [])
-    
-      const fetchAndRenderFeaturedHeroes = async () => {
-        let heroes = [];
-    
-        for (const heroId of featuredHeroesIds) {
-          const hero = await getBasicHeroInfoById(heroId);
-          heroes.push(hero);
-        }
-    
+    }, []);
+
+    const fetchAndRenderFeaturedHeroes = async () => {
+        const heroes = await Promise.all(
+            featuredHeroesIds.map(async (heroId) => {
+                const hero = await getBasicHeroInfoById(heroId);
+                return hero;
+            })
+        );
+
         setFeaturedHeroesList(heroes);
         setLoadingState(false);
-    }
+    };
 
     return (
         <section className='featured'>
             <h1>Featured Heroes</h1>
-            {
-                !isLoading && (
-                    <div className='featured__list'>
-                        {featuredHeroesList.map(({ id, name, powerstats, image }) => 
-                            <HeroSimplified key={id} heroId={id} name={name} powerstats={powerstats} image={image} />
-                        )}
-                    </div>
-                )
-            }
-            {
-                isLoading && (
-                    <div className='loader-container'>
-                        <Loader />
-                    </div>
-                )
-            }
+            {!isLoading ? (
+                <div className='featured__list'>
+                    {featuredHeroesList.map(({ id, name, powerstats, image }) => (
+                        <HeroSimplified
+                            key={id}
+                            heroId={id}
+                            name={name}
+                            powerstats={powerstats}
+                            image={image}
+                        />
+                    ))}
+                </div>
+            ) : (
+                <div className='loader-container'>
+                    <Loader />
+                </div>
+            )}
         </section>
     );
 }
