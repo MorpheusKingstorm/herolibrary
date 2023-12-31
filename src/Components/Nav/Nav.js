ca4cb64d9433cd7935fe1c07f034bbe659716c44
Oskar Grzelak <business.oskargrzelak@gmail.com>
2023-12-31 15:54:15 +0100

    Added Enter key search input handler, added some spaces to make code cleaner, changed styles in Nav

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -4,7 +4,14 @@ import logo from '../../assets/img/nav_logo.png';
 import { Link, Outlet } from 'react-router-dom';
 
 function Nav() {
-    const [searchInputValue, setSearchInputValue] = useState('');
+    const [ searchInputValue, setSearchInputValue ] = useState('');
+
+    const onEnterPress = (event) => {
+        if (event.key === 'Enter') {
+            setSearchInputValue(event.target.value);
+            window.location.href = `/search/${searchInputValue}`;
+        }
+    };
 
     return (
         <>
@@ -12,7 +19,7 @@ function Nav() {
                 <div className="container">
                     <Link to="/"><img className="nav__logo" src={logo} alt="Superhero Database" /></Link>
                     <div className="nav__search">
-                        <input onChange={event => {setSearchInputValue(event.target.value)}} value={searchInputValue} type="text" name="search" />
+                        <input onKeyDown={onEnterPress} onChange={event => {setSearchInputValue(event.target.value)}} value={searchInputValue} type="text" name="search" />
                         <Link to={`/search/${searchInputValue}`}><button>Find hero!</button></Link>
                     </div>
                 </div>
